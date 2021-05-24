a7494664d8bd73c720f77f51e533dfd43135462a
Matt Rothenberg <mattrothenberg@github.com>
2021-05-24 11:48:27 -0400

    feat: add action

---
+++ types.js
@@ -0,0 +1,39 @@
+const csv = require("csvtojson");
+const fs = require("fs");
+const {
+  quicktype,
+  InputData,
+  jsonInputForTargetLanguage,
+} = require("quicktype-core");
+
+async function quicktypeJSON(targetLanguage, typeName, jsonString) {
+  const jsonInput = jsonInputForTargetLanguage(targetLanguage);
+  await jsonInput.addSource({
+    name: typeName,
+    samples: [jsonString],
+  });
+
+  const inputData = new InputData();
+  inputData.addInput(jsonInput);
+
+  return await quicktype({
+    inputData,
+    lang: targetLanguage,
+  });
+}
+
+async function main() {
+  csv()
+    .fromFile("./data.csv")
+    .then(async (json) => {
+      const { lines } = await quicktypeJSON(
+        "ts",
+        "BoyBandDatum",
+        JSON.stringify(json)
+      );
+      const formatted = lines.join("\n");
+      fs.writeFileSync("./types.ts", formatted);
+    });
+}
+
+main();
